c1998078d1eece181cdcfbd8cb5f5c228ccef273
Gustavo de Lima Pereira <gustaprof25@gmail.com>
2026-05-13 11:04:20 -0300

    Atualizando o portifolio
    Adicionando categorias

--- js/script.js
+++ js/script.js
@@ -14,3 +14,41 @@ document.getElementById("formContato").addEventListener("submit", function(e) {
 
 
 });
+// FILTRO PORTFÓLIO
+
+const filterButtons = document.querySelectorAll('.filter-btn');
+const portfolioItems = document.querySelectorAll('.portfolio-item');
+
+filterButtons.forEach(button => {
+
+  button.addEventListener('click', () => {
+
+    // remove active
+    filterButtons.forEach(btn => btn.classList.remove('active'));
+
+    // adiciona active
+    button.classList.add('active');
+
+    const filter = button.getAttribute('data-filter');
+
+    portfolioItems.forEach(item => {
+
+      if(filter === 'all') {
+
+        item.style.display = 'block';
+
+      } else {
+
+        if(item.dataset.category === filter) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+
+      }
+
+    });
+
+  });
+
+});
