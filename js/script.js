1f27e8a5bfe23aa339a21655f2785bd314eb0a7e
Gustavo de Lima Pereira <gustaprof25@gmail.com>
2026-05-15 09:33:35 -0300

    Alterando o formulario

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-document.getElementById("formContato").addEventListener("submit", function(e) {
+document.getElementById("formContato").addEventListener("submit", function (e) {
   e.preventDefault();
 
   let nome = document.getElementById("nome").value;
   let email = document.getElementById("email").value;
+  let tipo = document.getElementById("tipo").value;
+  let data = document.getElementById("data").value;
 
-  let mensagem = `Olá, meu nome é ${nome} e meu email é ${email}  e gostaria de um orçamento!`;
-  let telefone = "5563992895005";
+  let dataFormatada = new Date(data).toLocaleDateString("pt-BR");
+
+  let mensagem = `Olá, meu nome é ${nome} e meu email é ${email}, meu evento é um ${tipo} e será no dia ${dataFormatada} e gostaria de um orçamento!`;
+  let telefone = "5563992093340";
 
   let url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
 
@@ -33,13 +37,13 @@ filterButtons.forEach(button => {
 
     portfolioItems.forEach(item => {
 
-      if(filter === 'all') {
+      if (filter === 'all') {
 
         item.style.display = 'block';
 
       } else {
 
-        if(item.dataset.category === filter) {
+        if (item.dataset.category === filter) {
           item.style.display = 'block';
         } else {
           item.style.display = 'none';
