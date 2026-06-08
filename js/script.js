cb23d70ad371eb68375db032c301336e69138f44
Gustavo de Lima Pereira <gustaprof25@gmail.com>
2026-06-08 09:54:47 -0300

    add cookies e politica de privacidade

--- js/script.js
+++ js/script.js
@@ -116,3 +116,28 @@ document
 setInterval(nextCard,5000);
 
 updateCoverflow();
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const banner =
+    document.getElementById('cookie-banner');
+
+    const button =
+    document.getElementById('accept-cookies');
+
+    if(localStorage.getItem('cookiesAccepted')){
+
+        banner.style.display = 'none';
+    }
+
+    button.addEventListener('click',()=>{
+
+        localStorage.setItem(
+            'cookiesAccepted',
+            'true'
+        );
+
+        banner.style.display='none';
+    });
+
+});
